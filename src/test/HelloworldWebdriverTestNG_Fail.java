e89e87ce492d5e09a74cfea40d73620452c92c02
eason99188 <zhanglei99188@163.com>
2015-04-18 23:07:36 +0800

    first commit

---
+++ src/test/HelloworldWebdriverTestNG_Fail.java
@@ -0,0 +1,52 @@
+package test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import org.testng.Assert;
+
+/**
+@author Bruce Gu
+**/
+public class HelloworldWebdriverTestNG_Fail {
+	
+	public  static final String SELENIUMCN_LINK = "//div[@id='content_left']/div[@id='1']//h3/a";
+
+	WebDriver driver;
+	
+	@Test
+	public void testOne_Fail() throws Exception {	
+		
+		driver = new ChromeDriver();
+		
+		driver.get("http://www.baidu.com");
+
+		
+		WebElement query = driver.findElement(By.name("wd"));
+		query.sendKeys("selenium 中文论坛");
+
+		WebElement btn = driver.findElement(By.id("su"));
+		btn.click();
+		Thread.sleep(5000);
+		
+
+		WebElement link = driver.findElement(By
+				.xpath(SELENIUMCN_LINK));
+		link.click();
+		Thread.sleep(5000);
+		//切换窗口
+		driver.switchTo().window(driver.getWindowHandles().toArray(new String[0])[1]);
+		
+		String expectedTitle ="Selenium 中文论坛, Webdriver 中文论坛, Selenium2, 软件测试,web自动化测试_Fail";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle,expectedTitle);		
+
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		driver.quit();
+	}
+	
+}
